8e4540e1e9566827b5617f0c5d0cdf80fe3d6118
Pete Inge <pinge@bluecadet.com>
2023-05-27 20:38:37 -0400

    Add in basic file saving.

--- src/Form/TimingMonitorSettings.php
+++ src/Form/TimingMonitorSettings.php
@@ -42,7 +42,39 @@ class TimingMonitorSettings extends ConfigFormBase {
       '#description' => $this->t('The maximum number of messages to keep in the database log. Requires a <a href=":cron">cron maintenance task</a>.', [':cron' => Url::fromRoute('system.status')->toString()]),
     ];
 
+    $form['directory'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Log File location'),
+      '#default_value' => $config->get('directory') ?? "tm_logs",
+      '#description' => $this->t('The location where logs are saved. Relative paths are inside the public files/ directory, but protected from web access. Should not start or end with a "/".'),
+      '#required' => TRUE,
+    ];
+
+    $form['gzip'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Compress archived files with gzip (EXPERIMENTAL)'),
+      '#default_value' => $config->get('gzip'),
+      '#description' => $this->t(''),
+    ];
+
     return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    // Check directory.
+    $dir_input = $form_state->getValue('directory');
+
+    $start_slash_pattern = "/^\//i";
+    if (preg_match_all($start_slash_pattern, $dir_input)) {
+      $form_state->setErrorByName('directory', "The log file location should not start with a slash.");
+    }
+    $end_slash_pattern = "/\/$/i";
+    if (preg_match_all($end_slash_pattern, $dir_input)) {
+      $form_state->setErrorByName('directory', "The log file location should not end with a slash.");
+    }
   }
 
   /**
@@ -51,6 +83,8 @@ class TimingMonitorSettings extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('timing_monitor.settings')
       ->set('row_limit', $form_state->getValue('row_limit'))
+      ->set('directory', $form_state->getValue('directory'))
+      ->set('gzip', $form_state->getValue('gzip'))
       ->save();
 
     parent::submitForm($form, $form_state);
---
+++ src/TimingMonitorUtility.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\timing_monitor;
+
+/**
+ * Add in Timing Monitor.
+ */
+class TimingMonitorUtility {
+
+  /**
+   * Craete a csv safe test string.
+   */
+  function csvEscape(array $data, $delimiter = ',') {
+    $buffer = fopen('php://temp', 'r+');
+    fputcsv($buffer, $data, $delimiter);
+    rewind($buffer);
+    $csv = fgets($buffer);
+    fclose($buffer);
+
+    return rtrim($csv);
+  }
+
+}
