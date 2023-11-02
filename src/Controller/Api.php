dbf23735ef76c46d2d695d52b4b799f048d9d1db
Pete Inge <pete.inge@gmail.com>
2023-11-01 23:24:38 -0400

    Add in Get Daily Averages

--- src/Controller/Api.php
+++ src/Controller/Api.php
@@ -6,6 +6,7 @@ use Drupal\Core\Cache\CacheableJsonResponse;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Database\Connection;
+use Drupal\timing_monitor\TimingMonitor;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -99,6 +100,88 @@ class Api extends ControllerBase implements ContainerInjectionInterface {
     $select->range(0, 50);
 
     $data['data'] = $select->execute()->fetchAll(\PDO::FETCH_ASSOC);
+
+    $response = new CacheableJsonResponse($data);
+    return $response;
+  }
+
+  /**
+   * Callback for daily avereages for a specific type.
+   */
+  public function dailyAverage(string $type, Request $request): CacheableJsonResponse | array {
+
+    $data = [
+      "status" => "OK",
+      "data" => [
+        'type' => $type,
+        'dates' => [],
+      ],
+    ];
+
+    // ksm($type);
+
+    // ksm($request->get('startDate'));
+    // ksm($request->get('endDate'));
+
+    $type_match = (strpos($type, "%") !== FALSE) ? "LIKE" : "=";
+    $today = new \DateTime();
+    $today->setTime(23, 59, 59);
+    $days = $request->get('days') ?? 7;
+    $start_day = $request->get('startDate') ?? $today->format('Y-m-d');
+    $end_date_obj = (clone $today)->modify("-$days days");
+    $end_date_obj->setTime(0, 0, 0);
+    $end_day = $request->get('endDate') ?? $end_date_obj->format('Y-m-d');
+
+    // ksm($start_day, $end_day, $today, $end_date_obj);
+
+    for ($i = 0; $i < $days; $i++) {
+      $dates[(clone $today)->modify("-$i days")->format('Y-m-d')] = NULL;
+    }
+
+
+
+
+    $select = $this->database->select('timing_monitor_log', 'tm')->fields('tm', []);
+    // ksm($this->database, $select);
+    // $select->addExpression("DATE_FORMAT(FROM_UNIXTIME(timestamp), :format)", 'date', [":format" => "%Y%m%d"]);
+    // $select->condition('type', $type);
+    // $select->condition('marker', TimingMonitor::FINISH);
+    // $select->groupBy('type', 'timestamp', 'd');
+    // $select->orderBy('id', 'DESC');
+    // $select->range(0, 50);
+    // $results = $select->execute()->fetchAll(\PDO::FETCH_ASSOC);
+
+    $query = $this->database->query('
+      SELECT t.date, AVG(duration) avg
+      FROM (
+        SELECT duration, DATE_FORMAT(FROM_UNIXTIME(timestamp), :format) date
+        FROM {timing_monitor_log}
+        WHERE type '. $type_match .' :type
+        AND marker = :marker
+        AND timestamp <= :start_date
+        AND timestamp >= :end_date
+      ) as t
+      GROUP BY date
+    ', [
+      ":format" => "%Y-%m-%d",
+      ":type" => $type,
+      ":marker" => TimingMonitor::FINISH,
+      ":start_date" => $today->format("U"),
+      ":end_date" => $end_date_obj->format("U"),
+    ]);
+    // ksm($query, $query->getQueryString());
+    // ksm($query->arguments());
+    $results = $query->fetchAll(\PDO::FETCH_ASSOC);
+    // ksm($results);
+
+    foreach ($results as $result) {
+      $dates[$result['date']] = (float) $result['avg'];
+    }
+
+    $data['data']['dates'] = $dates;
+
+    // ksm($data);
+    // return [];
 
     $response = new CacheableJsonResponse($data);
     return $response;
