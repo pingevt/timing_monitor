29b19741f02c3d2234a72df1b6e97e1dd0aa2180
Pete Inge <pete.inge@gmail.com>
2023-11-03 21:17:15 -0400

    Add in scaffold for tests

---
+++ tests/src/Unit/TimingMonitorTests.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\Tests\timing_monitor\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ *
+ */
+class TimingMonitortiming_monitorTest extends UnitTestCase {
+
+  /**
+   * Before a test method is run, setUp() is invoked.
+   * Create new unit object.
+   */
+  public function setUp() {
+
+  }
+
+  /**
+   * Test staic vars.
+   */
+  public function testCheckStaticVars() {
+    $this->assertTrue(TRUE);
+  }
+
+  /**
+   * Once test method has finished running, whether it succeeded or failed, tearDown() will be invoked.
+   * Unset the  object.
+   */
+  public function tearDown() {
+
+  }
+
+}
