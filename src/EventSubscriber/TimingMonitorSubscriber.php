3c9f648a47ce29978fc7f32908d528a471a9ce20
Pete Inge <pete.inge@gmail.com>
2023-11-12 15:47:56 -0500

    Add in 400, 404, and 403 error catches
    #11

--- src/EventSubscriber/TimingMonitorSubscriber.php
+++ src/EventSubscriber/TimingMonitorSubscriber.php
@@ -2,22 +2,129 @@
 
 namespace Drupal\timing_monitor\EventSubscriber;
 
+use Drupal\Core\EventSubscriber\HttpExceptionSubscriberBase;
 use Drupal\timing_monitor\TimingMonitor;
+use Drupal\Core\Logger\LoggerChannelTrait;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\KernelEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 
 /**
  * Add in Timing Monitor.
  */
-class TimingMonitorSubscriber implements EventSubscriberInterface {
+class TimingMonitorSubscriber extends HttpExceptionSubscriberBase implements EventSubscriberInterface {
+
+  use LoggerChannelTrait;
 
   /**
    * {@inheritdoc}
    */
-  public static function getSubscribedEvents() {
+  public static function getSubscribedEvents(): array {
+    $events = parent::getSubscribedEvents();
     // $events['kernel.request'] = ['onRequest', 28];
     $events['kernel.finish_request'] = ['finishRequest'];
     return $events;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static function getPriority() {
+    return 350;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getHandledFormats() {
+    return [];
+  }
+
+  /**
+   * Redirects on 400 Bad Request kernel exceptions.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+   *   The Event to process.
+   */
+  public function on400(RequestEvent $event) {
+
+    $request = $event->getRequest();
+    $exception = $event->getThrowable();
+
+    if (strpos($request->getRequestUri(), "/api/timing-monitor/") === 0 || $request->getRequestUri() == "/api/timing-monitor") {
+      $data = [
+        'status' => (int) $exception->getStatusCode(),
+        'error_msg' => 'Bad Request',
+        'full_msg' => $exception->getMessage(),
+      ];
+      $response = new JsonResponse($data);
+      $event->setResponse($response);
+
+      // Log this call.
+      $this->getLogger('timing_monitor')->error("400: Bad Api call. " . $exception->getMessage(), [
+        "request" => $request,
+        "exception" => $exception,
+      ]);
+    }
+  }
+
+  /**
+   * Redirects on 403 Access Denied kernel exceptions.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+   *   The Event to process.
+   */
+  public function on403(RequestEvent $event) {
+
+    $request = $event->getRequest();
+    $exception = $event->getThrowable();
+
+    if (strpos($request->getRequestUri(), "/api/timing-monitor/") === 0 || $request->getRequestUri() == "/api/timing-monitor") {
+
+      $data = [
+        'status' => (int) $exception->getStatusCode(),
+        'error_msg' => 'Access Denied',
+        // 'full_msg' => $exception->getMessage(), DEBUG ONLY
+      ];
+      $response = new JsonResponse($data);
+      $event->setResponse($response);
+
+      // Log this call.
+      $this->getLogger('timing_monitor')->error("403: Bad Api call. " . $exception->getMessage(), [
+        "request" => $request,
+        "exception" => $exception,
+      ]);
+    }
+  }
+
+  /**
+   * Redirects on 404 Not Found kernel exceptions.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+   *   The Event to process.
+   */
+  public function on404(RequestEvent $event) {
+
+    $request = $event->getRequest();
+    $exception = $event->getThrowable();
+
+    if (strpos($request->getRequestUri(), "/api/timing-monitor/") === 0 || $request->getRequestUri() == "/api/timing-monitor") {
+      $data = [
+        'status' => (int) $exception->getStatusCode(),
+        'error_msg' => 'Not Found',
+        'full_msg' => $exception->getMessage(),
+      ];
+      $response = new JsonResponse($data);
+      $event->setResponse($response);
+
+      // Log this call.
+      $this->getLogger('timing_monitor')->error("403: Bad Api call. " . $exception->getMessage(), [
+        "request" => $request,
+        "exception" => $exception,
+      ]);
+    }
+
   }
 
   // phpcs:disable
